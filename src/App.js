201019adf4b26f4937af62e40d16c71598735c76
plonp <40211693+plonp@users.noreply.github.com>
2024-11-23 17:57:55 +0800

    .

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      testing just
+    THIS IS MY FIRST REACT APP
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
